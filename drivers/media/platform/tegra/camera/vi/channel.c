6877c3f5bff625d39b4cf099b30e2e502518f1f9
Cosmin Tanislav <demonsingur@gmail.com>
2024-06-08 22:14:03 +0300

    media: tegra: zero-initialize structs containing stream member

--- drivers/media/platform/tegra/camera/vi/channel.c
+++ drivers/media/platform/tegra/camera/vi/channel.c
@@ -301,7 +301,7 @@ static void tegra_channel_fmts_bitmap_init(struct tegra_channel *chan)
 		.which = V4L2_SUBDEV_FORMAT_ACTIVE,
 	};
 
-	struct v4l2_subdev_pad_config pad_cfg;
+	struct v4l2_subdev_pad_config pad_cfg = { };
 	struct v4l2_subdev_state cfg = {.pads = &pad_cfg};
 	bitmap_zero(chan->fmts_bitmap, MAX_FORMAT_NUM);
 
@@ -1051,8 +1051,8 @@ tegra_channel_enum_framesizes(struct file *file, void *fh,
 {
 	struct tegra_channel *chan = video_drvdata(file);
 	struct v4l2_subdev *sd = chan->subdev_on_csi;
-	struct v4l2_subdev_frame_size_enum fse;
-	struct v4l2_subdev_pad_config pad_cfg;
+	struct v4l2_subdev_frame_size_enum fse = { };
+	struct v4l2_subdev_pad_config pad_cfg = { };
 	struct v4l2_subdev_state cfg = {.pads = &pad_cfg};
 	int ret = 0;
 
@@ -1081,8 +1081,8 @@ tegra_channel_enum_frameintervals(struct file *file, void *fh,
 {
 	struct tegra_channel *chan = video_drvdata(file);
 	struct v4l2_subdev *sd = chan->subdev_on_csi;
-	struct v4l2_subdev_frame_interval_enum fie;
-	struct v4l2_subdev_pad_config pad_cfg;
+	struct v4l2_subdev_frame_interval_enum fie = { };
+	struct v4l2_subdev_pad_config pad_cfg = { };
 	struct v4l2_subdev_state cfg = {.pads = &pad_cfg};
 	int ret = 0;
 
@@ -2038,9 +2038,9 @@ __tegra_channel_try_format(struct tegra_channel *chan,
 			struct v4l2_pix_format *pix)
 {
 	const struct tegra_video_format *vfmt;
-	struct v4l2_subdev_format fmt;
+	struct v4l2_subdev_format fmt = { };
 	struct v4l2_subdev *sd = chan->subdev_on_csi;
-	struct v4l2_subdev_pad_config pad_cfg;
+	struct v4l2_subdev_pad_config pad_cfg = { };
 	struct v4l2_subdev_state cfg = {.pads = &pad_cfg};
 	int ret = 0;
 
@@ -2087,9 +2087,9 @@ __tegra_channel_set_format(struct tegra_channel *chan,
 			struct v4l2_pix_format *pix)
 {
 	const struct tegra_video_format *vfmt;
-	struct v4l2_subdev_format fmt;
+	struct v4l2_subdev_format fmt = { };
 	struct v4l2_subdev *sd = chan->subdev_on_csi;
-	struct v4l2_subdev_pad_config pad_cfg;
+	struct v4l2_subdev_pad_config pad_cfg = { };
 	struct v4l2_subdev_state cfg = {.pads = &pad_cfg};
 	int ret = 0;
 
